8c3ec5f7c5078ac77273fd3e37aa161322bee02e
SozzZZ <zj32032@outlook.com>
2019-01-04 19:25:26 +0800

    sql

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-from flask import Flask,app,request,render_template
+from flask import Flask,app,request,render_template,session,redirect,url_for,flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -6,26 +6,60 @@ from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField , SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(Form):
     name = StringField('What is your name?',validators=[Required()])
     submit = SubmitField('Submit')
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User',backref='role',lazy='dynamic')
+    def __repr__(self):
+        return '<Role %s>' %self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %s>' %self.username
+
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name = None
     form  = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None :
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            flash('Looks like you have changed your name!')
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('index.html',current_time= datetime.utcnow(),form=form, name=name)
+        return redirect(url_for('index'))
+    return render_template('index.html',current_time= datetime.utcnow(),form=form, name=session.get('name'),known = session.get('known',False))
+
 
 @app.route('/request')
 def reque():
@@ -44,20 +78,9 @@ def start():
 def req():
     print('request==========================================')
 
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
